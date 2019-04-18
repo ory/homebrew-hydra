77a79dc678af036512f5fdd6db0492a8bcc351e4
ory <hi@ory.sh>
2019-04-18 12:54:36 +0200

    Brew formula update for hydra version v1.0.0-rc.9+oryOS.10

--- hydra.rb
+++ hydra.rb
@@ -2,9 +2,9 @@
 class Hydra < Formula
   desc ""
   homepage "https://www.ory.sh"
-  url "https://github.com/ory/hydra/releases/download/v1.0.0-rc.8+oryOS.10/hydra_1.0.0-rc.8+oryOS.10_macOS_64-bit.tar.gz"
-  version "1.0.0-rc.8+oryOS.10"
-  sha256 "9a397f79024bdca27e765ba2be97af7780d441005518de10c0b9387d6637ce7a"
+  url "https://github.com/ory/hydra/releases/download/v1.0.0-rc.9+oryOS.10/hydra_1.0.0-rc.9+oryOS.10_macOS_64-bit.tar.gz"
+  version "1.0.0-rc.9+oryOS.10"
+  sha256 "c49088dec4c08ec9fc1698e1a6a54e52451acf9e043b61fe9a06f03fc410e774"
 
   def install
     bin.install "hydra"
