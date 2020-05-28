d5ee0a4da58e34f58a8e05b12fe7e5794e0f6cf1
aeneasr <aeneas@ory.sh>
2020-05-28 15:59:19 +0200

    Brew formula update for hydra version v1.5.0-beta.5

--- hydra.rb
+++ hydra.rb
@@ -2,24 +2,24 @@
 class Hydra < Formula
   desc ""
   homepage "https://www.ory.sh"
-  version "1.5.0-beta.3"
+  version "1.5.0-beta.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ory/hydra/releases/download/v1.5.0-beta.3/hydra_1.5.0-beta.3_macOS_64-bit.tar.gz"
-    sha256 "ae83a142b56eff436318c9b36872043ec92e129e0f91f6c0d82e7d143289026f"
+    url "https://github.com/ory/hydra/releases/download/v1.5.0-beta.5/hydra_1.5.0-beta.5_macOS_64-bit.tar.gz"
+    sha256 "03d256ca4b455de904dbf0ea14c09fa51d1cf6656559d167f79bb72a244d81a5"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/ory/hydra/releases/download/v1.5.0-beta.3/hydra_1.5.0-beta.3_linux_64-bit.tar.gz"
-      sha256 "10b33e1e5074d36c6174f3886af795e00d89c9c7d74ed22bf0df0b39a1749f59"
+      url "https://github.com/ory/hydra/releases/download/v1.5.0-beta.5/hydra_1.5.0-beta.5_linux_64-bit.tar.gz"
+      sha256 "01981b9376f83db7817fa182d2b6e755a1449f7438c10cfb8d4b601c12b737ae"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/ory/hydra/releases/download/v1.5.0-beta.3/hydra_1.5.0-beta.3_linux_arm64.tar.gz"
-        sha256 "4955a63e62a18cf3f14cb2ef107a310a98f7c86f3662fd2846967fca4d4e9000"
+        url "https://github.com/ory/hydra/releases/download/v1.5.0-beta.5/hydra_1.5.0-beta.5_linux_arm64.tar.gz"
+        sha256 "a109aa10cfdd919246c81ddc3000877d81b902064d30da6d83238f70c1228690"
       else
-        url "https://github.com/ory/hydra/releases/download/v1.5.0-beta.3/hydra_1.5.0-beta.3_linux_armv6.tar.gz"
-        sha256 "96ff3e852007b96d86128cbdb2b72b7ad84b9f04aaa25eaed8b54dd1702c4f26"
+        url "https://github.com/ory/hydra/releases/download/v1.5.0-beta.5/hydra_1.5.0-beta.5_linux_armv6.tar.gz"
+        sha256 "0da4ad54f618f8ddf13fece2882935180c1c6a550f2be57f98a5e5f2e87b8a97"
       end
     end
   end
