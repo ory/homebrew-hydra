969db7889b551f5a8a2096df72c5c2a2286ec38b
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-01-12 14:22:02 +0100

    Brew formula update for hydra version v1.9.0-alpha.4.pre.0

--- hydra.rb
+++ hydra.rb
@@ -2,16 +2,16 @@
 class Hydra < Formula
   desc ""
   homepage "https://www.ory.sh"
-  version "1.9.0-alpha.3"
+  version "1.9.0-alpha.4.pre.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ory/hydra/releases/download/v1.9.0-alpha.3/hydra_1.9.0-alpha.3-sqlite_macos_64bit.tar.gz"
-    sha256 "eb59ba025a62c69695b43d6fa784fe6594d8e9e0b03dc1e6bbc1b4cbc363700a"
+    url "https://github.com/ory/hydra/releases/download/v1.9.0-alpha.4.pre.0/hydra_1.9.0-alpha.4.pre.0-sqlite_macos_64bit.tar.gz"
+    sha256 "084db020f0931856d9c21855d2a1e80df83be7d18c8bb779b218e4e59e6f7bcd"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/ory/hydra/releases/download/v1.9.0-alpha.3/hydra_1.9.0-alpha.3-sqlite_linux_64bit.tar.gz"
-      sha256 "f6fa8483d5da19e4e492a38d0bb237392f2617f437b013ac552892dcc95840e8"
+      url "https://github.com/ory/hydra/releases/download/v1.9.0-alpha.4.pre.0/hydra_1.9.0-alpha.4.pre.0-sqlite_linux_64bit.tar.gz"
+      sha256 "259800b4b2b133547073a6eb2e2a117650ba7110947a1af2860f2d8ce0954f24"
     end
   end
 
